f1f624b5482c29588d035ec9e17d0b55e1d767e6
divya <divyaswami13621@gmail.com>
2024-09-24 19:22:10 +0530

    add this files

---
+++ src/sep/ex_0130902024/lab0015.java
@@ -0,0 +1,15 @@
+package sep.ex_0130902024;
+
+public class lab0015 {
+    public static void main(String[] args) {
+        // Literals
+        // first type -
+        // Boolean value - true or false.
+        // second type -
+        // integer value- int = 24; decimal system
+        // third type -
+        // octal value - int8 is base
+        // four type-
+        // hexadecimal- int 16 is base
+    }
+}
